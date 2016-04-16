77d04d1b062c29d777e0c8ecea54f37fb2f5303a
Shaw Young <shawson@gmail.com>
2016-04-16 12:21:44 +0100

    work in progress
    extending base repo to use data annotations

--- src/SystemChecker.Model/Data/CheckToPerform.cs
+++ src/SystemChecker.Model/Data/CheckToPerform.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SystemChecker.Model.Data
 {
+    [Table("tblChecksToPerform")]
     public class CheckToPerform
     {
+        [Key]
         public int CheckId { get; set; }
         public int CheckTypeId { get; set; }
         public string SystemName { get; set; }
